0b67923e053a639531523f60823dbbbf1858500f
francoischalifour <francois.chalifour@gmail.com>
2017-06-11 21:39:46 +0100

    Add util function to separate thousands

--- src/util/index.js
+++ src/util/index.js
@@ -9,3 +9,17 @@ export const arrayIntersection = (a = [], b = []) => {
   const bSet = new Set(b)
   return a.filter(x => bSet.has(x))
 }
+
+/**
+ * Returns a number as a string with thousand separator.
+ *
+ * @param {number|string} number
+ * @param {string} separator
+ */
+export const numberWithSeparator = (number, separator = ' ') => {
+  if (typeof number !== 'number' && typeof number !== 'string') {
+    return number
+  }
+
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator)
+}
--- src/util/index.spec.js
+++ src/util/index.spec.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-import { arrayIntersection } from './index'
+import { arrayIntersection, numberWithSeparator } from './index'
 
 describe('arrayIntersection', () => {
   const tests = [{
@@ -24,4 +24,26 @@ describe('arrayIntersection', () => {
   tests.forEach(test => it(test.message, () => {
     expect(arrayIntersection(...test.args)).toEqual(test.expected)
   }))
+})
+
+describe('numberWithSeparator', () => {
+  it('Should return a formatted number with spaces from a number', () => {
+    expect(numberWithSeparator(1000000)).toEqual('1 000 000')
+  })
+
+  it('Should return a formatted number with spaces from a string', () => {
+    expect(numberWithSeparator('1000000')).toEqual('1 000 000')
+  })
+
+  it('Should return a formatted number with commas', () => {
+    expect(numberWithSeparator('1000000', ',')).toEqual('1,000,000')
+  })
+
+  it('Should return an unformatted number if lower than 1000', () => {
+    expect(numberWithSeparator('100')).toEqual('100')
+  })
+
+  it('Should return the default argument if not a number or a string', () => {
+    expect(numberWithSeparator([])).toEqual([])
+  })
 })
